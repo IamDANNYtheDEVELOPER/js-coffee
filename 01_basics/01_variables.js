12674c4bc7586b653136da7bd74ee6969d6f0330
IamDANNYtheDEVELOPER <95649183+IamDANNYtheDEVELOPER@users.noreply.github.com>
2024-04-06 14:35:46 +0000

    current

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 341341
+let accountEmail = "daniel@parker.com"
+var accountPassword = "0341"
+accountCity = "New York"
+let accountState
+let accountProvidence = null
+let accountActive = false
+
+accountEmail = "miles@parker.com"
+accountPassword = "0968"
+accountCity = "Queens"
+
+console.log(accountId); 
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState, accountProvidence, accountActive]);
